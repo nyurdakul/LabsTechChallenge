117ab52e51c1b1286c33b9eb63d1548ab9a96abb
nyurdakul <nazliyurdakul@gmail.com>
2018-01-17 23:40:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/ny2266')
+def nazli():
+	return render_template('ny2266.html')
 
 @app.route('/', methods=['GET'])
 def main():
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run()
